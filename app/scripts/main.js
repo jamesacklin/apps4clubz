2870cf84e98df3be153e4a8dd27fee22c974572a
James Acklin <jamesacklin@gmail.com>
2016-09-13 14:16:39 -0400

    Form submission with AJAX, additional usability improvements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,16 @@
 WebFont.load({
   google: {
     families: ['Roboto Slab:100,300,400,700', 'Roboto']
+  },
+  custom: {
+    families: ['FontAwesome'],
+    urls: ['//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css']
   }
 });
-
 var wow = new WOW({
   mobile: false
 });
-
 wow.init();
-
 $(document).ready(function() {
   $('.tour-button').click(function(e) {
     e.preventDefault();
@@ -19,7 +20,6 @@ $(document).ready(function() {
     }, 1000);
   });
 });
-
 $(window).scroll(function() {
   //stick nav to top of page
   var y = $(this).scrollTop();
@@ -30,27 +30,41 @@ $(window).scroll(function() {
     $('nav').removeClass('sticky');
   }
 });
-
 $.fn.serializeObject = function() {
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name]) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
+  var o = {};
+  var a = this.serializeArray();
+  $.each(a, function() {
+    if (o[this.name]) {
+      if (!o[this.name].push) {
+        o[this.name] = [o[this.name]];
+      }
+      o[this.name].push(this.value || '');
+    } else {
+      o[this.name] = this.value || '';
+    }
+  });
+  return o;
 };
-
 $(function() {
-    $('#contact-form').on('submit', function(e) {
-      e.preventDefault();
-      var formData = $(this).serializeObject();
-      console.log(formData);
+  $('#contact-form').on('submit', function(e) {
+    e.preventDefault();
+    var formData = $(this).serializeObject();
+    $('.button-submit').html('<i class="fa fa-fw fa-spin fa-spinner"></i> Please wait...');
+    $.ajax({
+      type: 'POST',
+      url: 'https://prod.apps4clubz.com/api/leads',
+      data: formData,
+      dataType: 'json',
+      encode: true
+    }).done(function(data) {
+      if (data.errors) {
+        $('.button-submit').html('<i class="fa fa-fw fa-exclamation-circle"></i> Please try submitting again.');
+        // Sneakily log errors, delete this for production
+        console.log(data.errors);
+      } else {
+        $('#contact-form input').prop('disabled', true).parent('.form-row').addClass('form-row-disabled');
+        $('.button-submit').html('<i class="fa fa-fw fa-check"></i> Thanks for getting in touch!');
+      }
     });
+  });
 });
